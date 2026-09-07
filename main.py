0e77d1ff8f5b1d12cf862a36bf9ed2d09cdb9255
Maksim Raspopov <raspopov_maksim@inbox.ru>
2026-09-07 07:49:47 +0500

    basic server

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import base64
 from contextlib import asynccontextmanager
 
 import uvicorn
-from Crypto.Cipher import PKCS1_OAEP
-from Crypto.PublicKey import RSA
+# from Crypto.Cipher import PKCS1_OAEP
+# from Crypto.PublicKey import RSA
 from fastapi import FastAPI
 from pydantic import BaseModel, ConfigDict
 from pydantic.alias_generators import to_camel
@@ -37,25 +37,25 @@ app.include_router(security.router)
 app.include_router(chats.router)
 app.include_router(fcm.router)
 
-private_key = RSA.generate(2048)
-public_key = private_key.publickey()
-
-PUBLIC_KEY_PEM = public_key.export_key()
-PRIVATE_KEY_PEM = private_key.export_key()
-
-
-def encrypt_aes_key_with_rsa(aes_key: bytes) -> str:
-    """Шифруем AES ключ с помощью RSA"""
-    cipher_rsa = PKCS1_OAEP.new(RSA.import_key(PUBLIC_KEY_PEM))
-    encrypted_key = cipher_rsa.encrypt(aes_key)
-    return base64.b64encode(encrypted_key).decode('utf-8')
-
-
-def decrypt_aes_key_with_rsa(encrypted_key_b64: str) -> bytes:
-    """Расшифровываем AES ключ с помощью RSA"""
-    encrypted_key = base64.b64decode(encrypted_key_b64)
-    cipher_rsa = PKCS1_OAEP.new(RSA.import_key(PRIVATE_KEY_PEM))
-    return cipher_rsa.decrypt(encrypted_key)
+# private_key = RSA.generate(2048)
+# public_key = private_key.publickey()
+#
+# PUBLIC_KEY_PEM = public_key.export_key()
+# PRIVATE_KEY_PEM = private_key.export_key()
+#
+#
+# def encrypt_aes_key_with_rsa(aes_key: bytes) -> str:
+#     """Шифруем AES ключ с помощью RSA"""
+#     cipher_rsa = PKCS1_OAEP.new(RSA.import_key(PUBLIC_KEY_PEM))
+#     encrypted_key = cipher_rsa.encrypt(aes_key)
+#     return base64.b64encode(encrypted_key).decode('utf-8')
+#
+#
+# def decrypt_aes_key_with_rsa(encrypted_key_b64: str) -> bytes:
+#     """Расшифровываем AES ключ с помощью RSA"""
+#     encrypted_key = base64.b64decode(encrypted_key_b64)
+#     cipher_rsa = PKCS1_OAEP.new(RSA.import_key(PRIVATE_KEY_PEM))
+#     return cipher_rsa.decrypt(encrypted_key)
 
 
 if __name__ == "__main__":
